5f4827a24d1ad7a6969a414944bf0fd5760eb369
Pranav Marathe <pmarathe25@gmail.com>
2016-12-03 17:03:19 -0500

    Completed prediction model.

--- stockAnalyzer.py
+++ stockAnalyzer.py
@@ -6,30 +6,54 @@ import datetime
 def growthProbability(stockName):
     stock = Share(stockName)
 
+    weightPE = 0.01
+    weightPEG = 0.01
+    weightShort = 0.01
+    weightHistorical = 0.01
+
     PERatio = stock.get_price_earnings_ratio()
     #  Scale PE Ratio
-    PERatio = 1 - 1 / math.exp(float(PERatio) / 40)
-    weightPE = 0.15
+    if (PERatio is not None):
+        PERatio = 1 - 1 / math.exp(float(PERatio) / 40)
+        weightPE = 0.15
+    else:
+        print "Could not retrieve PE ratio."
+        PERatio = 0
 
     PEGRatio = stock.get_price_earnings_growth_ratio()
     # Scale PEG Ratio
-    PEGRatio = (4 - float(PEGRatio)) / 4
-    weightPEG = 0.3
+    if (PEGRatio is not None):
+        PEGRatio = (2 - float(PEGRatio)) / 2
+        # Clamp PEG Ratio
+        PEGRatio = clamp(PEGRatio, 0, 1)
+        weightPEG = 0.3
+    else:
+        print "Could not retrieve PEG ratio."
+        PEGRatio = 0
 
     ShortRatio = stock.get_short_ratio()
     # Scale Short Ratio
-    ShortRatio = 1 - 1 / math.exp(float(ShortRatio) / 5)
-    weightShort = 0.15
+    if (ShortRatio is not None):
+        ShortRatio = 1 - 1 / math.exp(float(ShortRatio) / 5)
+        weightShort = 0.15
+    else:
+        print "Could not retrieve short ratio."
+        ShortRatio = 0
 
-    historical = getFiveDaySlope(stockName)
-    # Scale 5 day slope
-    historical = float(historical) / float(stock.get_price()) * 100
-    if (historical > 5.0):
-        historical = 5.0
-    historical = math.sin(historical)
-    weightHistorical = 0.4
+    try:
+        historical = getFiveDaySlope(stockName)
+        # Scale 5 day slope
+        historical = float(historical) / float(stock.get_price()) * 100
+        # Clamp historical price change between -100% and +5%
+        historical = clamp(historical, -5, 5)
+        historical = historical / 5 + math.sin(historical / 5)
+        weightHistorical = 0.4
+    except:
+        print "Could not retrieve historical data."
+        historical = 0
 
-    return PERatio * weightPE + PEGRatio * weightPEG + ShortRatio * weightShort + historical * weightHistorical
+    probability = (PERatio * weightPE + PEGRatio * weightPEG + ShortRatio * weightShort + historical * weightHistorical) / (weightPE + weightPEG + weightShort + weightHistorical)
+    return probability
 
 def getFiveDaySlope(stockName):
     # Initialize values.
@@ -50,3 +74,10 @@ def getFiveDaySlope(stockName):
     y = [float(stock.get_price()), float(stock.get_price())] + y
     slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
     return slope
+
+def clamp(value, lowerBound, upperBound):
+    if value < lowerBound:
+        value = lowerBound
+    elif value > upperBound:
+        value = upperBound
+    return value
