e4be3fba0f04e7c7732c327eb2d920d9bf3b4511
Pranav Marathe <pmarathe25@gmail.com>
2016-12-06 16:07:27 -0500

    Adds data parsing.

---
+++ parseStocks.py
@@ -0,0 +1,8 @@
+def parseFile(filename):
+    symbols = []
+    with open(filename) as f:
+        for line in f:
+            symbols.append(line.split('|')[1])
+    return symbols
+
+print parseFile('nasdaqtraded.txt')
