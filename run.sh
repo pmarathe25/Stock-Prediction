616fbbcc490368c7f2d6bf353b4709cdce608166
Pranav Marathe <pmarathe25@gmail.com>
2016-12-08 22:44:26 -0500

    Fixes run script

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 BROWSER=$(gconftool -g /desktop/gnome/url-handlers/http/command)
 export BROWSER="${BROWSER//"\"%s\""/}"
 python ./server.py &
-sensible-browser harambeinvestments1.html
+sensible-browser harambeinvestments.html
